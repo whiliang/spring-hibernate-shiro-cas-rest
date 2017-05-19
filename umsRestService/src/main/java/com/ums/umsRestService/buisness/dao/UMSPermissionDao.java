6c8ec9e1f39083813a8924743e5cc6c890aa8598
wanghuailiang <wanghuailiang@gqhmt.com>
2017-05-19 17:38:12 +0800

    cas-http

--- umsRestService/src/main/java/com/ums/umsRestService/buisness/dao/UMSPermissionDao.java
+++
@@ -1,9 +0,0 @@
-package com.ums.umsRestService.buisness.dao;
-
-import java.util.List;
-
-import com.ums.umsRestService.buisness.model.UMSPermission;
-import com.ums.umsRestService.common.dao.BaseDao;
-
-public interface UMSPermissionDao extends BaseDao<UMSPermission> {
-}
--- umsRestService/src/main/java/com/ums/umsRestService/buisness/service/PermissionService.java
+++
@@ -1,8 +0,0 @@
-package com.ums.umsRestService.buisness.service;
-
-import java.util.List;
-
-public interface PermissionService {
-    //通过一个角色名称的集合，找到权限的并集
-    public List<String> getPermissionNames(String userName,Long appId);
-}
